d354839d619cb18254c0f114bef68c3e1030add7
cemalokten <cemalokten@gmail.com>
2022-10-31 12:12:58 +0000

    Add part1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -12,32 +12,32 @@ const Content = ({content})=> {
   })
 }
 
-const Total = ({total}) => {
-  return (
-    <p>Number of exercises {total.reduce((acc, c) => acc + c)}</p>
-  )
+const Total = ({parts}) => {
+  return (<p>Number of exercises {parts.reduce((acc, c) => acc + c.exercises, 0)}</p>)
 }
 
 const App = () => {
  const course = 'Half Stack application development'
-  const part1 = {
+  const parts = [
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content content={[part1, part2, part3]}/>
-      <Total total={[part1.exercises, part2.exercises, part3.exercises]}/>
+      <Content content={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
