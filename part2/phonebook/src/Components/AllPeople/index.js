06cd4ef27510cc2dbd5525cc984462eb1f43308d
cemalokten <cemalokten@gmail.com>
2023-04-10 19:12:08 +0200

    Update id to _id

--- part2/phonebook/src/Components/AllPeople/index.js
+++ part2/phonebook/src/Components/AllPeople/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import SinglePerson from "../SinglePerson";
-const AllPeople = ({filterdPersons, handleDelete}) => {
-    return <>
-        <h2>Numbers</h2>
-        {filterdPersons.map((c) =>  <SinglePerson key={c.name} name={c.name} number={c.number} id={c.id} handleDelete={handleDelete}/>)}
+const AllPeople = ({ filterdPersons, handleDelete }) => {
+  return (
+    <>
+      <h2>Numbers</h2>
+      {filterdPersons.map((c) => (
+        <SinglePerson
+          key={c?.name ? c.name : 'No name'}
+          name={c?.name ? c.name : 'No name'}
+          number={c.number}
+          id={c._id}
+          handleDelete={handleDelete}
+        />
+      ))}
     </>
-}
+  );
+};
 
 export default AllPeople
