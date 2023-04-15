d177326ae8a4a49bd10881736fe8b9719cc20873
cemalokten <cemalokten@gmail.com>
2023-04-15 23:51:16 +0200

    Update error handling

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,129 +1,137 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import AllPeople from "./Components/AllPeople";
 import NewPerson from "./Components/NewPerson";
 import Search from "./Components/Search";
 import Notification from "./Components/Notification";
-import Services from './services/'
+import Services from "./services/";
 
 const App = () => {
-  const [allPersons, setAllPersons] = useState([])
-  const [person, setPerson] = useState({ name: '', number: '' })
-  const [newPerson, setNewPerson] = useState({ name: '', number: '' })
-  const [search, setSearch] = useState('')
-  const [notification, setNotification] = useState(null)
+  const [allPersons, setAllPersons] = useState([]);
+  const [person, setPerson] = useState({ name: "", number: "" });
+  const [newPerson, setNewPerson] = useState({ name: "", number: "" });
+  const [search, setSearch] = useState("");
+  const [notification, setNotification] = useState(null);
 
   useEffect(() => {
     const getPersons = async () => {
       try {
         const { data, error } = await Services.getPersons();
-        setAllPersons(data)
+        setAllPersons(data);
         if (error) {
           throw new Error(error);
         }
       } catch (error) {
-        console.error(error)
+        console.error(error);
       }
-    }
-    getPersons()
-  }, [newPerson])
+    };
+    getPersons();
+  }, [newPerson]);
 
   const addPerson = async () => {
     try {
       const { data, error } = await Services.addPerson(person);
-      if (error) {
-        throw new Error(error);
-      }
-      setNewPerson({ data })
-      setNotification({message: `Added ${data.name}`, type: 'notification'})
-      setTimeout(()=> setNotification(null), 5000)
+      setNewPerson({ data });
+      setNotification({ message: `Added ${data.name}`, type: "notification" });
+      setTimeout(() => setNotification(null), 5000);
     } catch (error) {
-      console.error(error)
+      setNotification({ message: error.response.data.error, type: "error" });
+      setTimeout(() => setNotification(null), 5000);
     }
-  }
+  };
 
   const deletePerson = async (id, name) => {
     try {
       const { data, error } = await Services.deletePerson(id);
-      console.log(data)
       if (error) {
         throw new Error(error);
       }
-      setNewPerson({ data })
-      setNotification({message: `The entry for ${name} has been deleted from the server`, type: 'notification'})
+      setNewPerson({ data });
+      setNotification({
+        message: `The entry for ${name} has been deleted from the server`,
+        type: "notification",
+      });
     } catch (error) {
-      setNotification({message: `The entry for ${name} has already been deleted from the server`, type: 'error'})
-      setTimeout(()=> setNotification(null), 5000)
-      console.error(error)
+      setNotification({
+        message: `The entry for ${name} has already been deleted from the server`,
+        type: "error",
+      });
+      setTimeout(() => setNotification(null), 5000);
     }
-  }
+  };
 
   const updatePerson = async (person, id) => {
     try {
       const { data, error } = await Services.updatePerson(person, id);
-      setNewPerson({ data })
+      setNewPerson({ data });
       if (error) {
         throw new Error(error);
       }
     } catch (error) {
-      console.error(error)
+      setNotification({ message: error.response.data.error, type: "error" });
+      setTimeout(() => setNotification(null), 5000);
     }
-  }
+  };
 
-  const handleDuplicate =  (person) => {
-     return allPersons.some(({ name }) => {
-      return name === person.name
+  const handleDuplicate = (person) => {
+    return allPersons.some(({ name }) => {
+      return name === person.name;
     });
-  }
+  };
 
   const getUserByName = (person) => {
-    return allPersons.filter((c) => c.name === person.name)[0]
-  }
+    return allPersons.filter((c) => c.name === person.name)[0];
+  };
 
   const filteredPersons = allPersons?.filter(({ name }) => {
-    return name ? name.toLowerCase().includes(search.toLowerCase()) : 'n/a'
-  })
-
+    return name ? name.toLowerCase().includes(search.toLowerCase()) : "n/a";
+  });
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     if (handleDuplicate(person)) {
-      if (window.confirm('The user already exists, update the phone number?')) {
-        const { id }= getUserByName(person)
-        return updatePerson(person, id)
+      if (window.confirm("The user already exists, update the phone number?")) {
+        const { _id } = getUserByName(person);
+        return updatePerson(person, _id);
       }
     } else {
-      await addPerson(person)
-    }}
-
+      await addPerson(person);
+    }
+  };
 
   const handleNewName = (e) => {
-    setPerson({ ...person, name: e.target.value })
-  }
+    setPerson({ ...person, name: e.target.value });
+  };
 
   const handleNewNumber = (e) => {
-    setPerson({ ...person, number: e.target.value })
-  }
+    setPerson({ ...person, number: e.target.value });
+  };
 
   const handleSearch = (e) => {
-    setSearch(e.target.value)
-  }
+    setSearch(e.target.value);
+  };
 
   const handleDelete = (e, id, name) => {
-    e.preventDefault()
-    if (window.confirm('Are you sure you want to delete this entry?')) return deletePerson(id, name)
-  }
+    e.preventDefault();
+    if (window.confirm("Are you sure you want to delete this entry?"))
+      return deletePerson(id, name);
+  };
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <Notification notification={notification}/>
+      <Notification notification={notification} />
       <form>
         <Search handleSearch={handleSearch} search={search} />
-        <NewPerson handleNewName={handleNewName} handleNewNumber={handleNewNumber} handleSubmit={handleSubmit} newPerson={person} />
+        <NewPerson
+          handleNewName={handleNewName}
+          handleNewNumber={handleNewNumber}
+          handleSubmit={handleSubmit}
+          newPerson={person}
+        />
       </form>
       <AllPeople filterdPersons={filteredPersons} handleDelete={handleDelete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part2/phonebook/src/services/index.js
+++ part2/phonebook/src/services/index.js
@@ -1,41 +1,46 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:3001/api/persons'
+const BASE_URL = "http://localhost:3001/api/persons";
 
 const getPersons = async () => {
   try {
-    const res = await axios.get(BASE_URL)
-    return res
+    const res = await axios.get(BASE_URL);
+    return res;
   } catch (e) {
-    if (e.response) console.log(`There has been an error getting ${e.response.data}`)
+    if (e.response)
+      console.log(`There has been an error getting ${e.response.data}`);
   }
-}
+};
 
 const addPerson = async (newPerson) => {
   try {
-    const res = await axios.post(BASE_URL, newPerson)
-    return res
+    const res = await axios.post(BASE_URL, newPerson);
+    return res;
   } catch (e) {
-    if (e.response) console.log(`There has been an error adding ${e.response.data}`)
+    throw e
   }
-}
+};
 
 const deletePerson = async (id) => {
   try {
-    const res = await axios.delete(`${BASE_URL}/delete/${id}`)
-    return res
+    const res = await axios.delete(`${BASE_URL}/delete/${id}`);
+    return res;
   } catch (e) {
-    if (e.response) console.log(`There has been an error deleting ${e.response.data}`)
+    if (e.response)
+      console.log(`There has been an error deleting ${e.response.data}`);
   }
-}
+};
 
 const updatePerson = async (person, id) => {
   try {
-    const res = await axios.put(`http://localhost:3001/persons/${id}`, person)
-    return res
+    console.log(id, 'updatePerson')
+    console.log(person, 'updatePerson')
+    const res = await axios.put(`${BASE_URL}/update/${id}`, person);
+    return res;
   } catch (e) {
-    if (e.response) console.log(`There has been an error updating ${e.response.data}`)
+    if (e.response)
+      console.log(e)
   }
-}
+};
 
-export default { getPersons, addPerson, deletePerson , updatePerson}
+export default { getPersons, addPerson, deletePerson, updatePerson };
