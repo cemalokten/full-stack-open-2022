db856e524aa003e8f2f7763e873d764e1c9d0ec8
cemalokten <cemalokten@gmail.com>
2023-05-07 18:34:46 +0100

    Refactor into separate modules

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,54 +1,16 @@
 const express = require('express');
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('blog-list', blogSchema)
-
-const connectDB = async () => {
-  try {
-    const url = process.env.MONGODB_URI;
-    mongoose.set("strictQuery", false);
-    await mongoose.connect(url);
-    console.log("Connected to the database");
-  } catch (err) {
-    console.error("Error connecting to the database:", err);
-    process.exit(1);
-  }
-};
+const connectDB = require('./utils/connect')
+const config = require('./utils/config')
+const router = require('./controllers/blog')
 
 connectDB()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api', router)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
---
+++ part4/blog-list/utils/connect.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const config = require('./config')
+
+const connectDB = async () => {
+  try {
+    const url = config.MONGODB_URI;
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(url);
+    console.log("Connected to the database");
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB
