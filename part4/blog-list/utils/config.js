51679bdfd36de18e7d6f82418392b22b716fe70b
cemalokten <cemalokten@gmail.com>
2023-05-10 20:37:06 +0100

    Update config to accept test env

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI
+const { PORT } = process.env;
+const MONGODB_URI = process.env.NODE_ENV === 'test' ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI;
 
 module.exports = {
-  PORT, MONGODB_URI
-}
+  PORT, MONGODB_URI,
+};
