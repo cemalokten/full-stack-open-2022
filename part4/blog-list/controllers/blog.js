ee620427365755e43c385539d35f46bb70e6ddf6
cemalokten <cemalokten@gmail.com>
2023-07-10 15:31:06 +0100

    Update blog router

--- part4/blog-list/controllers/blog.js
+++ part4/blog-list/controllers/blog.js
@@ -1,26 +1,26 @@
 const blog = require('express').Router()
 const Blog = require('../models/blog')
 
-blog.get('/blogs', async (request, response) => {
+blog.get('/', async (request, response) => {
   const { limit } = request.query
   const blogs = await Blog.find({}).limit(limit)
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blog.post('/blogs', async (request, response) => {
+blog.post('/', async (request, response) => {
   if (!request.body.title || !request.body.url) response.status(400).end()
   const blog = new Blog(request.body)
   const result = await blog.save()
   response.status(201).json(result)
 })
 
-blog.delete('/blogs/delete/:id', async (request, response) => {
+blog.delete('/delete/:id', async (request, response) => {
   const _id = request.params?.id
   await Blog.deleteOne({_id})
   response.status(201).end()
 })
 
-blog.patch('/blogs/update/:id', async (request, response) => {
+blog.patch('/update/:id', async (request, response) => {
   // Get id
   const _id = request.params?.id
   await Blog.updateOne({_id}, request.body)
